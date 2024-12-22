af78701102c9494a604abc43bce5e976b8065ddc
Chris <42316927+xiansnn@users.noreply.github.com>
2024-12-22 18:06:54 +0100

    initiate rotary_encoder

---
+++ src/device_rotary_encoder/rotary_encoder.cpp
@@ -0,0 +1,39 @@
+#include "rotary_encoder.h"
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
+#include <string>
+
+RotaryEncoder::RotaryEncoder(uint encoder_clk_gpio, uint encoder_dt_gpio, 
+                            gpio_irq_callback_t call_back,
+                            control_event_processor_t event_processor,
+                            struct_SwitchButtonConfig clk_conf)
+    : SwitchButtonWithIRQ(encoder_clk_gpio, call_back, clk_conf)//, UIController()
+{
+    this->dt_gpio = encoder_dt_gpio;
+    this->active_lo = true;
+    this->event_processor = event_processor;
+
+    gpio_init(this->dt_gpio);
+    gpio_pull_up(this->dt_gpio);
+}
+
+RotaryEncoder::~RotaryEncoder()
+{
+}
+
+void RotaryEncoder::interrupt_service_routine(uint32_t current_irq_event_mask)
+{
+    irq_enabled(false);
+    ControlEvent clk_event = process_IRQ_event(current_irq_event_mask);
+    if (clk_event == ControlEvent::PUSH)
+    {
+        bool clockwise_rotation = gpio_get(dt_gpio);
+        if (clockwise_rotation)
+            // this->current_controlled_object->process_control_event(ControlEvent::INCREMENT);
+            event_processor(ControlEvent::INCREMENT);
+        else
+            // this->current_controlled_object->process_control_event(ControlEvent::DECREMENT);
+            event_processor(ControlEvent::DECREMENT);
+    }
+    irq_enabled(true);
+}
---
+++ tests/test_rotary_encoder.cpp
@@ -0,0 +1,78 @@
+/**
+ * @file test_rotary_encoder.cpp
+ * @author xiansnn (xiansnn@hotmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-12-22
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "rotary_encoder.h"
+#include "probe.h"
+#include <map>
+#include <string>
+
+#define CENTRAL_SWITCH_GPIO 6
+#define ENCODER_CLK_GPIO 26
+#define ENCODER_DT_GPIO 21
+
+// Probe pr_D1 = Probe(1); // copy of : encoder_clk.is_button_active()
+// Probe pr_D2 = Probe(2); // copy of : encoder_central_sw.is_button_active()
+Probe pr_D3 = Probe(3); // sw_call_back is running
+Probe pr_D4 = Probe(4); // clk_event != SwitchButtonEvent::NOOP
+Probe pr_D5 = Probe(5); // bounces discarded
+// channel 0 : central switch pin
+// channel 6 : encoder DT pin
+// channel 7 : encoder clk pin
+
+struct_SwitchButtonConfig cfg_encoder_clk{
+    .debounce_delay_us = 1000,
+};
+
+std::map<ControlEvent, std::string> event_to_string{
+    {ControlEvent::NOOP, "NOOP"},
+    {ControlEvent::PUSH, "PUSH"},
+    {ControlEvent::DOUBLE_PUSH, "DOUBLE_PUSH"},
+    {ControlEvent::LONG_PUSH, "LONG_PUSH"},
+    {ControlEvent::RELEASED_AFTER_LONG_TIME, "RELEASED_AFTER_LONG_TIME"},
+    {ControlEvent::RELEASED_AFTER_SHORT_TIME, "RELEASED_AFTER_SHORT_TIME"},
+    {ControlEvent::INCREMENT, "INCREMENT"},
+    {ControlEvent::DECREMENT, "DECREMENT"},
+    {ControlEvent::TIME_OUT, "TIME_OUT"}};
+
+void sw_call_back(uint gpio, uint32_t event_mask);
+
+void event_processor(ControlEvent event)
+{
+    printf("Encoder event(%s) \n", event_to_string[event].c_str());
+};
+
+RotaryEncoder encoder = RotaryEncoder(ENCODER_CLK_GPIO, ENCODER_DT_GPIO,
+                                      &sw_call_back, &event_processor, cfg_encoder_clk);
+
+// SwitchButtonWithIRQ encoder_clk = SwitchButtonWithIRQ(ENCODER_CLK_GPIO, &sw_call_back, cfg_encoder_clk);
+
+void sw_call_back(uint gpio, uint32_t event_mask)
+{
+    pr_D3.hi();
+
+    if (gpio == ENCODER_CLK_GPIO)
+        encoder.interrupt_service_routine(event_mask);
+    pr_D3.lo();
+}
+
+;
+
+int main()
+{
+    stdio_init_all();
+
+    while (true)
+    {
+        sleep_ms(10);
+    }
+
+    return 0;
+}
