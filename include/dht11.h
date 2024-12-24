33a2b2da627f22d0ae7e0de933db949df3f5161a
Chris <42316927+xiansnn@users.noreply.github.com>
2024-12-24 18:34:22 +0100

    add comment

--- include/dht11.h
+++ include/dht11.h
@@ -22,19 +22,37 @@
  */
 typedef struct
 {
+    /// @brief a flag that indicates the reading from DHT11 is valid
     bool is_valid;
+    /// @brief the converted values in % of relative humidity
     float humidity;
+    /// @brief the converted value in °Celsius
     float temp_celsius;
 } dht_reading_t;
 
 
+/**
+ * @brief the Class manages the DHT11 humidity and temperature sensor
+ * 
+ */
 class DHT11
 {
 private:
+    /// @brief the GPIO connected to DHT11
     uint gpio_in;
 
 public:
+    /**
+     * @brief the member used to read and convert the values send by DHT11
+     * 
+     * @param result according to dht_reading_t structure
+     */
     void read_from_dht(dht_reading_t *result);
+    /**
+     * @brief Construct a new DHT11 object
+     * 
+     * @param gpio_in_ the GPIO connected to DHT11
+     */
     DHT11(uint gpio_in_);
 };
 
