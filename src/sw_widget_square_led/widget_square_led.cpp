a8c3be64f1d96e4ad34c8e97bbdb80bdbbdfc4cf
Chris <42316927+xiansnn@users.noreply.github.com>
2025-01-13 18:14:16 +0100

    fixed destructor implementation

--- src/sw_widget_square_led/widget_square_led.cpp
+++ src/sw_widget_square_led/widget_square_led.cpp
@@ -1,16 +1,17 @@
 #include "widget_square_led.h"
 
 WidgetSquareLed::WidgetSquareLed(DisplayDevice *display_screen,
-                         size_t width, size_t height,
+                         size_t frame_width, size_t frame_height,
                          uint8_t widget_anchor_x, uint8_t widget_anchor_y,
                          bool widget_with_border,
                          FramebufferColor color, FramebufferFormat framebuffer_format)
-    : Widget(display_screen, width, height, widget_anchor_x, widget_anchor_y, widget_with_border, 1, framebuffer_format)
+    : Widget(display_screen, frame_width, frame_height, widget_anchor_x, widget_anchor_y, widget_with_border, 1, framebuffer_format)
 {
 }
 
 WidgetSquareLed::~WidgetSquareLed()
 {
+    delete display_screen;
 }
 
 void WidgetSquareLed::light_on()
