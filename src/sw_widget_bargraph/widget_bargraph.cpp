d6858ef91956bbcd1ffabeec9da3542de1991203
Chris <42316927+xiansnn@users.noreply.github.com>
2025-01-21 17:42:01 +0100

    initiate vertical bargraph

--- src/sw_widget_bargraph/widget_bargraph.cpp
+++ src/sw_widget_bargraph/widget_bargraph.cpp
@@ -83,7 +83,6 @@ WidgetHorizontalBargraph::WidgetHorizontalBargraph(ModelBargraph *bargraph_model
     int min_value = ((ModelBargraph *)actual_displayed_model)->min_value;
     int max_value = ((ModelBargraph *)actual_displayed_model)->max_value;
 
-    // this->bar_height = std::max(5, (uint8_t)this->widget_height / bin_quantity); // less than 5 px height is hard to read!
     this->bar_height = (widget_height - (bin_quantity + 1) * bar_spacing) / bin_quantity; // less than 5 px height is hard to read!
 
     this->widget_height = this->bar_height * bin_quantity + (bin_quantity + 1) * bar_spacing; // adjust effective widget height to an exact multiple of bin height + bin_spacing
@@ -118,17 +117,44 @@ void ModelBargraph::process_control_event(UIControlEvent event)
     this->set_change_flag();
 }
 
-uint8_t WidgetVerticalBargraph::convert_level_value_to_py(int level) // TODO
+uint8_t WidgetVerticalBargraph::convert_level_value_to_py(int level)
 {
-    return 0;
+    uint8_t position = level * level_coef + level_offset;
+    position = std::min(py_max, std::max(py_min, position));
+    return position;
 }
 
-void WidgetVerticalBargraph::draw() // TODO
+void WidgetVerticalBargraph::draw()
 {
+    for (int i = 0; i < ((ModelBargraph *)this->actual_displayed_model)->number_of_bar; i++)
+        draw_bar(i);
 }
 
-void WidgetVerticalBargraph::draw_bar(uint8_t bin_number) // TODO
+void WidgetVerticalBargraph::draw_bar(uint8_t bin_number)
 {
+    uint8_t bar_start_x = widget_start_x + bar_spacing + bin_number * (bar_width + bar_spacing);
+    rect(bar_start_x, widget_start_y, widget_width, bar_height, true, FramebufferColor::BLACK); // erase the bar area
+
+    uint8_t py = convert_level_value_to_py(((ModelBargraph *)this->actual_displayed_model)->values[bin_number]);
+    uint16_t p0 = convert_level_value_to_py(0);
+
+    uint8_t bar_start;
+    uint8_t bar_end;
+    if (((ModelBargraph *)this->actual_displayed_model)->values[bin_number] >= 0)
+    {
+        bar_start = py;
+        bar_end = p0;
+    }
+    else
+    {
+        bar_start = p0;
+        bar_end = py;
+    }
+
+    if (((ModelBargraph *)this->actual_displayed_model)->values[bin_number] == 0)
+        rect(bar_start_x, p0, bar_width, 1, true);
+    else
+        rect(bar_start_x, py, bar_width, bar_end - bar_start, true);
 }
 
 WidgetVerticalBargraph::WidgetVerticalBargraph(ModelBargraph *bargraph_model,
@@ -136,16 +162,41 @@ WidgetVerticalBargraph::WidgetVerticalBargraph(ModelBargraph *bargraph_model,
                                                size_t frame_width, size_t frame_height,
                                                uint8_t widget_anchor_x, uint8_t widget_anchor_y,
                                                bool widget_with_border,
-                                               uint8_t bar_spacing, uint8_t widget_border_width,
+                                               uint8_t _bar_spacing, uint8_t widget_border_width,
                                                FramebufferFormat framebuffer_format, struct_FramebufferText framebuffer_txt_cnf)
     : Widget(display_screen, frame_width, frame_height, widget_anchor_x, widget_anchor_y, widget_with_border, widget_border_width, framebuffer_format, framebuffer_txt_cnf)
-{ // TODO
+{
+    set_actual_displayed_object(bargraph_model);
+    this->bar_spacing = _bar_spacing;
+
+    uint8_t bin_quantity = ((ModelBargraph *)actual_displayed_model)->number_of_bar;
+    int min_value = ((ModelBargraph *)actual_displayed_model)->min_value;
+    int max_value = ((ModelBargraph *)actual_displayed_model)->max_value;
+
+    this->bar_width = (widget_width - (bin_quantity + 1) * bar_spacing) / bin_quantity; // less than 5 px height is hard to read!
+
+    this->widget_width = this->bar_width * bin_quantity + (bin_quantity + 1) * bar_spacing; // adjust effective widget height to an exact multiple of bin height + bin_spacing
+
+    this->py_max = this->widget_height;
+    this->py_min = this->widget_start_y;
+    this->bar_height = py_max - py_min;
+    this->level_coef = (float)(py_max - py_min) / (max_value - min_value);
+    this->level_offset = py_max - level_coef * max_value;
 }
 
-WidgetVerticalBargraph::~WidgetVerticalBargraph() // TODO
+WidgetVerticalBargraph::~WidgetVerticalBargraph()
 {
 }
 
-void WidgetVerticalBargraph::draw_refresh() // TODO
+void WidgetVerticalBargraph::draw_refresh()
 {
+    assert(this->actual_displayed_model != nullptr);
+
+    if (this->actual_displayed_model->has_changed())
+    {
+        this->draw();
+        this->draw_border();
+        this->display_screen->show(this, this->widget_anchor_x, this->widget_anchor_y);
+    }
+    this->actual_displayed_model->clear_change_flag();
 }
