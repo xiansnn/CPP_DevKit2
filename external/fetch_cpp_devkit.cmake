69afc98d470711e990aa2ab01ef418b8cca96851
Chris <42316927+xiansnn@users.noreply.github.com>
2025-03-14 19:04:00 +0100

    fix external fetch_cpp_devkit.cmake

--- external/fetch_cpp_devkit.cmake
+++ external/fetch_cpp_devkit.cmake
@@ -3,14 +3,15 @@
 # it must be inserted after the cmake project() command
 
 #====  defining CPP_DevKit src and include  ===============================
-set(CPP_DEVKIT_PATH "C:/Users/chris/OneDrive/MyPico_DevKit/CPP_DevKit/src")
-include_directories(${CPP_DevKit_SOURCE_DIR}/include) # used to define include directories 
+set(CPP_DEVKIT_PATH "C:/Users/chris/OneDrive/MyPico_DevKit/CPP_DevKit")
+include_directories(${CPP_DEVKIT_PATH}/include) # used to define include directories 
 
 #====  importing source  ==================================================
 include(FetchContent)
-FetchContent_Declare(CPP_DevKit SOURCE_DIR ${CPP_DEVKIT_PATH})
+FetchContent_Declare(CPP_DevKit SOURCE_DIR ${CPP_DEVKIT_PATH}/src)
 FetchContent_MakeAvailable(CPP_DevKit)
 
 #====  can be removed  ====================================================
-message("----->CPP_DevKit_SOURCE_DIR/src : ${CPP_DevKit_SOURCE_DIR}/src")   
-message("----->PROJECT_SOURCE_DIR : ${PROJECT_SOURCE_DIR}")                 
+message("----->CPP_DevKit_SOURCE_DIR/src : ${CPP_DEVKIT_PATH}/src")   
+message("----->CPP_DevKit_SOURCE_DIR/include : ${CPP_DEVKIT_PATH}/include")   
+message("----->PROJECT_SOURCE_DIR : ${PROJECT_SOURCE_DIR}")  
