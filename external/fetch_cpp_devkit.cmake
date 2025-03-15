2966064ea316c74d2a954ead22a937a856e7152d
Chris <42316927+xiansnn@users.noreply.github.com>
2025-03-15 17:44:26 +0100

    fix external fetch_cpp_devkit.cmake

--- external/fetch_cpp_devkit.cmake
+++ external/fetch_cpp_devkit.cmake
@@ -3,15 +3,16 @@
 # it must be inserted after the cmake project() command
 
 #====  defining CPP_DevKit src and include  ===============================
-set(CPP_DEVKIT_PATH "C:/Users/chris/OneDrive/MyPico_DevKit/CPP_DevKit")
-include_directories(${CPP_DEVKIT_PATH}/include) # used to define include directories 
+set(CPP_DEVKIT_SRC_PATH "C:/Users/chris/OneDrive/MyPico_DevKit/CPP_DevKit/src")
+set(CPP_DEVKIT_INC_PATH "C:/Users/chris/OneDrive/MyPico_DevKit/CPP_DevKit/include")
+message("-freq->CPP_DEVKIT_SRC_PATH : ${CPP_DEVKIT_SRC_PATH}")  
+
+#====  setting include directory  ==================================================
+message("-freq->CPP_DEVKIT_INC_PATH : ${CPP_DEVKIT_INC_PATH}")   
+include_directories(${CPP_DEVKIT_INC_PATH}) # used to define include directories 
+
 
 #====  importing source  ==================================================
 include(FetchContent)
-FetchContent_Declare(CPP_DevKit SOURCE_DIR ${CPP_DEVKIT_PATH}/src)
+FetchContent_Declare(CPP_DevKit SOURCE_DIR ${CPP_DEVKIT_SRC_PATH})
 FetchContent_MakeAvailable(CPP_DevKit)
-
-#====  can be removed  ====================================================
-message("----->CPP_DevKit_SOURCE_DIR/src : ${CPP_DEVKIT_PATH}/src")   
-message("----->CPP_DevKit_SOURCE_DIR/include : ${CPP_DEVKIT_PATH}/include")   
-message("----->PROJECT_SOURCE_DIR : ${PROJECT_SOURCE_DIR}")  
