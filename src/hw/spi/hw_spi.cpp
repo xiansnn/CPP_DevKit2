03ae528b9f5d2734199abb979a5389c8c6a447ce
Chris <42316927+xiansnn@users.noreply.github.com>
2025-04-05 16:43:58 +0200

    add set format before all spi write

--- src/hw/spi/hw_spi.cpp
+++ src/hw/spi/hw_spi.cpp
@@ -18,38 +18,45 @@ HW_SPI_Master::~HW_SPI_Master()
 
 int HW_SPI_Master::burst_write_8(uint8_t *src, size_t len)
 {
+    spi_set_format(spi, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
     return spi_write_blocking(spi, src, len);
 }
 
 int HW_SPI_Master::burst_write_16(uint16_t *src, size_t len)
 {
-    return spi_write16_blocking(spi,src,len);
+    spi_set_format(spi, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+    return spi_write16_blocking(spi, src, len);
 }
 
 int HW_SPI_Master::burst_write_read_8(uint8_t *src, uint8_t *dest, size_t len)
 {
+    spi_set_format(spi, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
     return spi_write_read_blocking(spi, src, dest, len);
 }
 
 int HW_SPI_Master::single_write_8(uint8_t byte_value)
 {
     uint8_t bytes[1] = {byte_value};
+    spi_set_format(spi, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
     return spi_write_blocking(spi, bytes, 1);
 }
 
 int HW_SPI_Master::single_write_16(uint16_t word_value)
 {
-    uint16_t words[1]= {word_value};
-    return spi_write16_blocking(spi, words,1);
+    spi_set_format(spi, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+    uint16_t words[1] = {word_value};
+    return spi_write16_blocking(spi, words, 1);
 }
 
 int HW_SPI_Master::single_read_8(uint8_t *dest)
 {
+    spi_set_format(spi, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
     return spi_read_blocking(spi, 0x00, dest, 1);
 }
 
 int HW_SPI_Master::burst_read_8(uint8_t repeated_tx_data, uint8_t *dest, size_t len)
 {
+    spi_set_format(spi, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
     return spi_read_blocking(spi, repeated_tx_data, dest, len);
 }
 
