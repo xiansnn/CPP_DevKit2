1139dbc481a3f7bdd40c7777e9b9ce370f53b134
Chris <42316927+xiansnn@users.noreply.github.com>
2025-04-13 16:08:33 +0200

    complete test of graphic primitives : -> OK

--- tests/sw/widget/test_st7735_graphic_primitives.cpp
+++ tests/sw/widget/test_st7735_graphic_primitives.cpp
@@ -59,8 +59,8 @@ public:
 };
 my_full_screen_widget::my_full_screen_widget(GraphicDisplayDevice *graphic_display_screen,
                                              struct_ConfigGraphicWidget graph_cfg, CanvasFormat format)
-    : GraphicWidget(graphic_display_screen, graph_cfg, format) { }
-my_full_screen_widget::~my_full_screen_widget() { }
+    : GraphicWidget(graphic_display_screen, graph_cfg, format) {}
+my_full_screen_widget::~my_full_screen_widget() {}
 void my_full_screen_widget::get_value_of_interest() {}
 void my_full_screen_widget::draw() {};
 
@@ -68,21 +68,24 @@ void test_fb_line(ST7735 *display)
 {
     display->clear_device_screen_buffer();
     my_full_screen_widget frame = my_full_screen_widget(display, full_screen_cfg, CANVAS_FORMAT);
-    for (int i = 2; i < 5; i++)
+
+    int i = 0;
+    for (int x = 0; x < 128; x++)
     {
-        ColorIndex c = static_cast<ColorIndex>(i);
+        i++;
+        if (i > 20 * 8)
+            i = 1;
+        frame.line(x, 0, 128 - 1 - x, 128 - 1, static_cast<ColorIndex>(i / 8));
+        frame.show();
+    }
 
-        for (int x = 0; x < 128; x++)
-        {
-            frame.line(x, 0, 128 - 1 - x, 128 - 1, c);
-            frame.show();
-        }
-
-        for (int y = 128 - 1; y >= 0; y--)
-        {
-            frame.line(0, y, 128 - 1, 128 - 1 - y, c);
-            frame.show();
-        }
+    for (int y = 128 - 1; y >= 0; y--)
+    {
+        i++;
+        if (i > 20 * 8)
+            i = 1;
+        frame.line(0, y, 128 - 1, 128 - 1 - y, static_cast<ColorIndex>(i / 8));
+        frame.show();
     }
 }
 void test_outofframe_line(ST7735 *display)
@@ -91,7 +94,7 @@ void test_outofframe_line(ST7735 *display)
     int y0, x1, y1;
     display->clear_device_screen_buffer();
     x1 = 64;
-    y1 = 70;
+    y1 = 160;
     y0 = -10;
 
     uint8_t i = 0;
@@ -99,9 +102,9 @@ void test_outofframe_line(ST7735 *display)
     for (int x = -10; x < 138; x++)
     {
         i++;
-        if (i > 20)
+        if (i > 20 * 8)
             i = 0;
-        ColorIndex c = static_cast<ColorIndex>(i);
+        ColorIndex c = static_cast<ColorIndex>(i / 8);
         frame.line(x, y0, x1, y1, c);
         frame.show();
     }
@@ -111,12 +114,12 @@ void test_fb_rect(ST7735 *display)
     my_full_screen_widget frame = my_full_screen_widget(display, full_screen_cfg, CANVAS_FORMAT);
 
     display->clear_device_screen_buffer();
-    frame.rect(0, 0, 128, 64);
+    frame.rect(0, 0, 128, 64, false, ColorIndex::RED);
+    frame.show();
+    sleep_ms(500);
+    frame.rect(10, 10, 108, 44, true, ColorIndex::YELLOW);
     frame.show();
     sleep_ms(1000);
-    frame.rect(10, 10, 108, 44, true);
-    frame.show();
-    sleep_ms(2000);
 }
 void test_fb_hline(ST7735 *display)
 {
@@ -129,7 +132,7 @@ void test_fb_hline(ST7735 *display)
         frame.show();
     }
     frame.show(); // 80ms @ bitrate 10Mbps
-    sleep_ms(500);
+    sleep_ms(1000);
 }
 void test_fb_vline(ST7735 *display)
 {
@@ -142,7 +145,45 @@ void test_fb_vline(ST7735 *display)
         frame.show();
     }
     frame.show(); // 80ms @ bitrate 10Mbps
+    sleep_ms(1000);
+}
+
+void test_fb_circle(ST7735 *display)
+{
+    my_full_screen_widget frame = my_full_screen_widget(display, full_screen_cfg, CANVAS_FORMAT);
+
+    display->clear_device_screen_buffer();
+    frame.circle(50, 63, 31, false, ColorIndex::ORANGE);
+    frame.show();
     sleep_ms(500);
+    frame.circle(20, 64, 32, true, ColorIndex::LIME);
+    frame.show();
+    sleep_ms(1000);
+}
+
+void test_fb_in_fb(ST7735 *display)
+{
+    my_full_screen_widget frame = my_full_screen_widget(display, full_screen_cfg, CANVAS_FORMAT);
+
+    display->clear_device_screen_buffer();
+    frame.rect(0, 0, display->TFT_panel_width_in_pixel, display->TFT_panel_height_in_pixel);
+    frame.rect(10, 10, 108, 44, true, ColorIndex::CYAN);
+    frame.line(5, 60, 120, 5, ColorIndex::RED);
+    frame.show();
+    sleep_ms(500);
+
+    struct_ConfigGraphicWidget small_frame_cfg{
+        .pixel_frame_width = 80,
+        .pixel_frame_height = 24,
+        .widget_anchor_x = 24,
+        .widget_anchor_y = 24};
+
+    my_full_screen_widget small_frame = my_full_screen_widget(display, small_frame_cfg, CANVAS_FORMAT);
+    small_frame.canvas->fill_canvas_with_color(ColorIndex::NAVY);
+    small_frame.line(5, 5, 80, 20, ColorIndex::YELLOW);
+    small_frame.circle(8, 44, 12, false, ColorIndex::GREEN);
+    small_frame.show();
+    sleep_ms(1000);
 }
 
 int main()
@@ -154,13 +195,13 @@ int main()
 
     while (true)
     {
-        // test_fb_line(&display);
-        // test_outofframe_line(&display);
-        // test_fb_hline(&display);
+        test_fb_line(&display);
+        test_outofframe_line(&display);
+        test_fb_hline(&display);
         test_fb_vline(&display);
-        // test_fb_rect(&display);
-        // test_fb_circle(&display);
-        // test_fb_in_fb(&display);
+        test_fb_rect(&display);
+        test_fb_circle(&display);
+        test_fb_in_fb(&display);
     }
     return 0;
 }
