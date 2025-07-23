7beb8cd6aabc263955e35273bea506c4a746e2b8
Chris <42316927+xiansnn@users.noreply.github.com>
2025-07-23 18:26:24 +0200

    First test with xTaskCreate

--- tests/utilities/probe/test_async_debug_probe.cpp
+++ tests/utilities/probe/test_async_debug_probe.cpp
@@ -8,7 +8,11 @@
  * @copyright Copyright (c) 2023
  *
  */
+#include "FreeRTOS.h"
 
+#include "task.h"
+#include <stdio.h>
+#include "pico/stdlib.h"
 #include "pico/time.h"
 #include "utilities/probe/probe.h"
 
@@ -23,35 +27,117 @@ Probe p7 = Probe(7);
 
 Probe *probes[] = {&p0, &p1, &p2, &p3, &p4, &p5, &p6, &p7};
 
+void probe_sequence_0(void *)
+{
+    while (true)
+    {
+        p0.hi();
+        vTaskDelay(1);
+        p0.lo();
+        vTaskDelay(10);
+    }
+}
+void probe_sequence_1(void *)
+{
+    while (true)
+    {
+        p1.hi();
+        vTaskDelay(1);
+        p1.lo();
+        vTaskDelay(20);
+    }
+}
+void probe_sequence_2(void *)
+{
+    while (true)
+    {
+        p2.hi();
+        vTaskDelay(1);
+        p2.lo();
+        vTaskDelay(30);
+    }
+}
+void probe_sequence_3(void *)
+{
+    while (true)
+    {
+        p3.hi();
+        vTaskDelay(1);
+        p3.lo();
+        vTaskDelay(40);
+    }
+}
+void probe_sequence_4(void *)
+{
+    while (true)
+    {
+        p4.hi();
+        vTaskDelay(1);
+        p4.lo();
+        vTaskDelay(50);
+    }
+}
+void probe_sequence_5(void *)
+{
+    while (true)
+    {
+        p5.hi();
+        vTaskDelay(1);
+        p5.lo();
+        vTaskDelay(60);
+    }
+}
+void probe_sequence_6(void *)
+{
+    while (true)
+    {
+        p6.hi();
+        vTaskDelay(1);
+        p6.lo();
+        vTaskDelay(70);
+    }
+}
+void probe_sequence_7(void *)
+{
+    while (true)
+    {
+        p7.hi();
+        vTaskDelay(1);
+        p7.lo();
+        vTaskDelay(80);
+    }
+}
+
 void probe_sequence(void *)
 {
-    for (size_t i = 0; i < 8; i++)
+    while (true)
     {
-        sleep_us(100);
-        probes[i]->hi();
-        sleep_us(10);
-        probes[i]->lo();
+        for (size_t i = 2; i < 8; i++)
+        {
+            vTaskDelay(100);
+            probes[i]->hi();
+            vTaskDelay(10);
+            probes[i]->lo();
+        }
+        vTaskDelay(1000);
     }
 }
 
 int main()
 {
-    /**
-     * @brief SETUP: Initialisation of a set of Probe, one for each channel.
-     */
+    // xTaskCreate(probe_sequence, "probe_sequence", 256, NULL, 1, NULL);
+    xTaskCreate(probe_sequence_0, "probe_sequence_0", 256, NULL, 2, NULL);
+    xTaskCreate(probe_sequence_1, "probe_sequence_1", 256, NULL, 1, NULL);
+    xTaskCreate(probe_sequence_2, "probe_sequence_2", 256, NULL, 1, NULL);
+    xTaskCreate(probe_sequence_3, "probe_sequence_3", 256, NULL, 1, NULL);
+    xTaskCreate(probe_sequence_4, "probe_sequence_4", 256, NULL, 1, NULL);
+    xTaskCreate(probe_sequence_5, "probe_sequence_5", 256, NULL, 1, NULL);
+    xTaskCreate(probe_sequence_6, "probe_sequence_6", 256, NULL, 1, NULL);
+    xTaskCreate(probe_sequence_7, "probe_sequence_7", 256, NULL, 1, NULL);
+    vTaskStartScheduler();
 
-    /**
-     * @brief LOOP: run an infinite loop.
-     *
-     */
     while (true)
     {
-        probe_sequence(nullptr);
-        sleep_us(2000);
-    }
-    /**
-     * @brief ENDLOOP:
-     *
-     */
-    return 0;
+        tight_loop_contents();
+    };
 }
