6ae6c4fd239663f5561185865e70cd7d0225f469
Chris <42316927+xiansnn@users.noreply.github.com>
2025-07-25 18:49:36 +0200

    use task function with parameters

--- test_freeRTOS/test_creating_task/test_async_debug_probe.cpp
+++ test_freeRTOS/test_creating_task/test_async_debug_probe.cpp
@@ -26,114 +26,39 @@ Probe p6 = Probe(6);
 Probe p7 = Probe(7);
 
 Probe *probes[] = {&p0, &p1, &p2, &p3, &p4, &p5, &p6, &p7};
+int n_delay[8] = {0, 1, 2, 3, 4, 5, 6, 7};
 
-void probe_sequence_0(void *)
+void probe_sequence_0(void *pxDelay)
 {
+    volatile int *delay = static_cast<int *>(pxDelay);
     while (true)
     {
-        p0.hi();
-        // vTaskDelay(1);
-        p0.lo();
-        // vTaskDelay(10);
+        probes[*delay]->hi();
+        probes[*delay]->lo();
     }
 }
-void probe_sequence_1(void *)
+void probe_sequence_1(void *pxDelay)
 {
+    volatile int *delay = static_cast<int *>(pxDelay);
     while (true)
     {
-        p1.hi();
-        // vTaskDelay(1);
-        p1.lo();
-        // vTaskDelay(20);
-    }
-}
-void probe_sequence_2(void *)
-{
-    while (true)
-    {
-        p2.hi();
+        probes[*delay]->hi();
         vTaskDelay(1);
-        p2.lo();
-        vTaskDelay(30);
-    }
-}
-void probe_sequence_3(void *)
-{
-    while (true)
-    {
-        p3.hi();
-        vTaskDelay(1);
-        p3.lo();
-        vTaskDelay(40);
-    }
-}
-void probe_sequence_4(void *)
-{
-    while (true)
-    {
-        p4.hi();
-        vTaskDelay(1);
-        p4.lo();
-        vTaskDelay(50);
-    }
-}
-void probe_sequence_5(void *)
-{
-    while (true)
-    {
-        p5.hi();
-        vTaskDelay(1);
-        p5.lo();
-        vTaskDelay(60);
-    }
-}
-void probe_sequence_6(void *)
-{
-    while (true)
-    {
-        p6.hi();
-        vTaskDelay(1);
-        p6.lo();
-        vTaskDelay(70);
-    }
-}
-void probe_sequence_7(void *)
-{
-    while (true)
-    {
-        p7.hi();
-        vTaskDelay(1);
-        p7.lo();
-        vTaskDelay(80);
-    }
-}
-
-void probe_sequence(void *)
-{
-    while (true)
-    {
-        for (size_t i = 2; i < 8; i++)
-        {
-            vTaskDelay(100);
-            probes[i]->hi();
-            vTaskDelay(10);
-            probes[i]->lo();
-        }
-        vTaskDelay(1000);
+        probes[*delay]->lo();
+        vTaskDelay((*delay + 1) * 10);
     }
 }
 
 int main()
 {
-    // xTaskCreate(probe_sequence, "probe_sequence", 256, NULL, 1, NULL);
-    xTaskCreate(probe_sequence_0, "pr_sequence_0", 256, NULL, 1, NULL);
-    xTaskCreate(probe_sequence_1, "pr_sequence_1", 256, NULL, 1, NULL);
-    xTaskCreate(probe_sequence_2, "pr_sequence_2", 256, NULL, 2, NULL);
-    xTaskCreate(probe_sequence_3, "pr_sequence_3", 256, NULL, 2, NULL);
-    xTaskCreate(probe_sequence_4, "pr_sequence_4", 256, NULL, 2, NULL);
-    xTaskCreate(probe_sequence_5, "pr_sequence_5", 256, NULL, 2, NULL);
-    xTaskCreate(probe_sequence_6, "pr_sequence_6", 256, NULL, 2, NULL);
-    xTaskCreate(probe_sequence_7, "pr_sequence_7", 256, NULL, 2, NULL);
+    xTaskCreate(probe_sequence_0, "pr_sequence_00", 256, &n_delay[0], 1, NULL);
+    xTaskCreate(probe_sequence_0, "pr_sequence_01", 256, &n_delay[1], 1, NULL);
+    xTaskCreate(probe_sequence_1, "pr_sequence_12", 256, &n_delay[2], 2, NULL);
+    xTaskCreate(probe_sequence_1, "pr_sequence_13", 256, &n_delay[3], 2, NULL);
+    xTaskCreate(probe_sequence_1, "pr_sequence_14", 256, &n_delay[4], 2, NULL);
+    xTaskCreate(probe_sequence_1, "pr_sequence_15", 256, &n_delay[5], 2, NULL);
+    xTaskCreate(probe_sequence_1, "pr_sequence_16", 256, &n_delay[6], 2, NULL);
+    xTaskCreate(probe_sequence_1, "pr_sequence_17", 256, &n_delay[7], 2, NULL);
     vTaskStartScheduler();
 
     while (true)
