a6aa887a844e03d842e4fe7b67c7af03c1bbdc6e
Chris <42316927+xiansnn@users.noreply.github.com>
2025-07-31 17:28:46 +0200

    make queue usage more clean

--- test_freeRTOS/test_queue/test_rtos_queue.cpp
+++ test_freeRTOS/test_queue/test_rtos_queue.cpp
@@ -27,66 +27,81 @@ Probe p7 = Probe(7);
 
 struct config_periodic_task
 {
-    int probe_number;
-    uint32_t period;
+    Probe *probe;
+    uint32_t period_ms;
     uint32_t task_duration_ms;
 };
 
-Probe *probes[] = {&p0, &p1, &p2, &p3, &p4, &p5, &p6, &p7};
-int n_delay[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+// Probe *probes[] = {&p0, &p1, &p2, &p3, &p4, &p5, &p6, &p7};
+// uint8_t n_probe[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+QueueHandle_t msg_queue = xQueueCreate(8, sizeof(uint8_t));
 
-config_periodic_task cnf_task = {
-    .probe_number = 1,
-    .period = 250,
-    .task_duration_ms = 5};
+config_periodic_task cnf_sending_task = {
+    .probe = &p1,
+    .period_ms = 1000,
+    .task_duration_ms = 1};
 
-void probe_sequence_full_load(void *pxDelay)
+void vIdleTask(void *pxProbe)
 {
-    volatile int *delay = static_cast<int *>(pxDelay);
     while (true)
     {
-        probes[*delay]->hi();
-        probes[*delay]->lo();
+        ((Probe *)pxProbe)->hi();
+        ((Probe *)pxProbe)->lo();
     }
 }
-// void probe_sequence_delay(void *pxDelay)
-// {
-//     volatile int *delay = static_cast<int *>(pxDelay);
-//     while (true)
-//     {
-//         probes[*delay]->hi();
-//         vTaskDelay(1);
-//         probes[*delay]->lo();
-//         vTaskDelay((*delay + 1) * 10);
-//     }
-// }
-void probe_sequence_periodic(void *pxPeriod)
+
+void vReceivingTask(void *pxProbe)
 {
-    const volatile config_periodic_task *config = static_cast<config_periodic_task *>(pxPeriod);
+    uint8_t cN;
+    while (true)
+    {
+        ((Probe *)pxProbe)->pulse_us(1);
+        if (uxQueueMessagesWaiting(msg_queue) != 0)
+        {
+            xQueueReceive(msg_queue, &cN, pdMS_TO_TICKS(100));
+            for (size_t i = 0; i < cN; i++)
+            {
+                ((Probe *)pxProbe)->pulse_us(10);
+                sleep_us(10);
+            }
+        }
+        ((Probe *)pxProbe)->pulse_us(1);
+        vTaskDelay(2);
+    }
+}
+void vPeriodicSendingTask(void *pxConfig)
+{
+    config_periodic_task *pxConfigPeriodicTask = (config_periodic_task *)pxConfig;
     TickType_t xLastWakeTime = xTaskGetTickCount();
-    size_t count = config->task_duration_ms * 2946;
+    size_t count = pxConfigPeriodicTask->task_duration_ms * 2946;
+    uint8_t delay;
     while (true)
     {
         for (size_t i = 0; i < count; i++)
         {
-            probes[config->probe_number]->hi();
-            probes[config->probe_number]->lo();
+            pxConfigPeriodicTask->probe->hi();
+            pxConfigPeriodicTask->probe->lo();
+        };
+        for (size_t j = 0; j < 8; j++)
+        {
+            delay = j + 1;
+            xQueueSend(msg_queue, &delay, 0);
         }
-        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(config->period));
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(pxConfigPeriodicTask->period_ms));
     }
 }
 
 int main()
 {
+    xTaskCreate(vIdleTask, "idle_task0", 256, &p0, 0, NULL);
+    xTaskCreate(vPeriodicSendingTask, "sending_task1", 256, &cnf_sending_task, 3, NULL);
+    xTaskCreate(vReceivingTask, "receiving_task2", 256, &p2, 2, NULL);
+    xTaskCreate(vReceivingTask, "receiving_task3", 256, &p3, 2, NULL);
+    xTaskCreate(vReceivingTask, "receiving_task4", 256, &p4, 2, NULL);
+    xTaskCreate(vReceivingTask, "receiving_task5", 256, &p5, 2, NULL);
+    xTaskCreate(vReceivingTask, "receiving_task6", 256, &p6, 2, NULL);
+    xTaskCreate(vReceivingTask, "receiving_task7", 256, &p7, 2, NULL);
 
-    xTaskCreate(probe_sequence_full_load, "idle_task", 256, &n_delay[0], 0, NULL);
-    xTaskCreate(probe_sequence_periodic, "periodic_task", 250, &cnf_task, 1, NULL);
-    // xTaskCreate(probe_sequence_full_load, "pr_sequence_01", 256, &n_delay[1], 1, NULL);
-    // xTaskCreate(probe_sequence_delay, "pr_sequence_12", 256, &n_delay[2], 2, NULL);
-    // xTaskCreate(probe_sequence_delay, "pr_sequence_13", 256, &n_delay[3], 2, NULL);
-    // xTaskCreate(probe_sequence_delay, "pr_sequence_14", 256, &n_delay[4], 2, NULL);
-    // xTaskCreate(probe_sequence_delay, "pr_sequence_15", 256, &n_delay[5], 2, NULL);
-    // xTaskCreate(probe_sequence_delay, "pr_sequence_16", 256, &n_delay[6], 2, NULL);
     vTaskStartScheduler();
 
     while (true)
