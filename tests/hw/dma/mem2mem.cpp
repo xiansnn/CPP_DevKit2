8c82ed71837a3aaaa0dd39e643380f586f36798e
Chris <42316927+xiansnn@users.noreply.github.com>
2025-09-14 19:16:23 +0200

    done

--- tests/hw/dma/mem2mem.cpp
+++ tests/hw/dma/mem2mem.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file mem2mem.cpp
+ * @author xiansnn (xiansnn@hotmail.com)
+ * @brief no benefit to use FreeRTOS for mem2me DMA -> way too fast 
+ * @version 0.1
+ * @date 2025-09-14
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hw/dma/hw_dma.h"
@@ -8,6 +18,7 @@ Probe p0 = Probe(0);
 Probe p1 = Probe(1);
 Probe p2 = Probe(2);
 Probe p3 = Probe(3);
+Probe p6 = Probe(6);
 
 #define TEST_SIZE 1024
 
@@ -15,15 +26,18 @@ void m2m_dma_handler();
 
 HW_DMA dma = HW_DMA(DMA_IRQ_0, m2m_dma_handler);
 
-// static struct_ConfigDMA cfg_dma{
-//     .transfer_size = DMA_SIZE_16,
-//     .number_of_transfer = TEST_SIZE,
-//     .dma_irq_handler = m2m_dma_handler,
-//     .irq_number = DMA_IRQ_0,
-// };
-
 static uint16_t read_buf[TEST_SIZE];
 static uint16_t write_buf[TEST_SIZE];
+
+struct mem2mem_data_queue
+{
+    uint32_t length;
+    uint16_t *source;
+    uint16_t *destination;
+};
+
+QueueHandle_t m2m_data_queue = xQueueCreate(3, sizeof(mem2mem_data_queue));
+SemaphoreHandle_t data_ready = xSemaphoreCreateBinary();
 
 void m2m_dma_handler()
 {
@@ -31,27 +45,59 @@ void m2m_dma_handler()
     {
         p2.hi();
         dma_hw->ints0 = (1u << dma.channel); // Clear IRQ
+        xSemaphoreGiveFromISR(dma.end_of_xfer, NULL);
         p2.lo();
     }
 }
 
-int main()
+void vIdleTask(void *pxProbe)
 {
-    stdio_init_all();
-
-    for (uint i = 0; i < TEST_SIZE; ++i)
-        read_buf[i] = i;
 
     while (true)
     {
-        p0.hi();
-        dma.xfer_mem2mem(TEST_SIZE, DMA_SIZE_16,write_buf,read_buf,false);
+        ((Probe *)pxProbe)->hi();
+        ((Probe *)pxProbe)->lo();
+    }
+}
+void vPeriodic_data_generation_task(void *param)
+{
+    mem2mem_data_queue data_to_send = {
+        .length = TEST_SIZE,
+        .source = read_buf,
+        .destination = write_buf,
+    };
+
+    while (true)
+    {
+        vTaskDelay(pdMS_TO_TICKS((uint32_t)param));
         p1.hi();
-        dma.start_dma();
-        dma_channel_wait_for_finish_blocking(dma.channel);
+        for (uint i = 0; i < TEST_SIZE; ++i)
+            read_buf[i] = i;
         p1.lo();
 
+        xQueueSend(m2m_data_queue, &data_to_send, portMAX_DELAY);
+    }
+}
+void vXchange_task(void *param)
+{
+    mem2mem_data_queue received_data;
+    while (true)
+    {
+        xQueueReceive(m2m_data_queue, &received_data, portMAX_DELAY);
         p3.hi();
+        dma.xfer_mem2mem(received_data.length, DMA_SIZE_16,
+                         received_data.destination, received_data.source, true);
+        p3.lo();
+        xSemaphoreTake(dma.end_of_xfer, portMAX_DELAY);
+        xSemaphoreGive(data_ready);
+    }
+}
+void vChecking_task(void *param)
+{
+    while (true)
+    {
+        xSemaphoreTake(data_ready, portMAX_DELAY);
+        p6.hi();
         for (uint i = 0; i < TEST_SIZE; ++i)
         {
             if (write_buf[i] != read_buf[i])
@@ -60,12 +106,24 @@ int main()
                       i, TEST_SIZE, read_buf[i], write_buf[i]);
             }
         }
+        p6.lo();
         printf("All good\n");
-        p3.lo();
-
-        // dma.cleanup_and_free_dma_channel();
-        p0.lo();
-
-        sleep_ms(500);
     }
+}
+
+int main()
+{
+    stdio_init_all();
+
+    xTaskCreate(vIdleTask, "idle_task0", 256, &p0, 0, NULL);
+    xTaskCreate(vPeriodic_data_generation_task, "compute_data", 250, (void *)500, 2, NULL);
+    xTaskCreate(vXchange_task, "m2m", 250, NULL, 4, NULL);
+    xTaskCreate(vChecking_task, "check_data", 250, NULL, 3, NULL);
+    vTaskStartScheduler();
+
+    while (true)
+    {
+        tight_loop_contents();
+    }
+    return 0;
 }
