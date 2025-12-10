ae02617889b63abd6012c1af32d54e286bb08075
Chris <42316927+xiansnn@users.noreply.github.com>
2025-12-10 18:15:02 +0100

    change graph appareance

--- tests/sw/rtos_widget/t_rtos_common_text_and_graph_widgets.cpp
+++ tests/sw/rtos_widget/t_rtos_common_text_and_graph_widgets.cpp
@@ -58,8 +58,8 @@ void my_visu_widget::draw()
 
     this->drawer->canvas->fill_canvas_with_color(drawer->canvas->bg_color);
 
-    this->drawer->circle(radius, xc, yl, true, ColorIndex::WHITE);
-    this->drawer->line(x0, y0, x1, y1, ColorIndex::BLACK);
+    this->drawer->circle(radius, xc, yl, false, ColorIndex::WHITE);
+    this->drawer->line(x0, y0, x1, y1, ColorIndex::WHITE);
 
     drawer->draw_border(drawer->canvas->fg_color);
 }
