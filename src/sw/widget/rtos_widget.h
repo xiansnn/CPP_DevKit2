aeb2bc4184d1542346ec5974d59f229a67c4a9d0
Chris <42316927+xiansnn@users.noreply.github.com>
2026-01-19 17:26:14 +0100

    move Blinker and BlinkingWidget to rtos_Widget library

--- src/sw/widget/rtos_widget.h
+++ src/sw/widget/rtos_widget.h
@@ -23,7 +23,47 @@
 #include "queue.h"
 #include "semphr.h"
 
-//TODO consider Blinker rtos implementation
+#include <set>
+
+class rtos_BlinkingWidget;
+class rtos_Blinker
+{
+private:
+    /// @brief The period of the blinking, in milliseconds
+    std::set<rtos_BlinkingWidget *> blinking_widgets;
+
+public:
+    uint32_t blink_period_ms;
+    bool current_blink_phase{false};
+    rtos_Blinker(uint32_t blink_period_ms);
+    ~rtos_Blinker();
+    void add_blinking_widget(rtos_BlinkingWidget *widget);
+    void remove_blinking_widget(rtos_BlinkingWidget *widget);
+    void refresh_blinking();
+};
+
+class rtos_BlinkingWidget
+{
+private:
+
+protected:
+    rtos_Blinker *blinker;
+    ColorIndex fg_color_backup;
+    ColorIndex bg_color_backup;
+
+public:
+    rtos_BlinkingWidget();
+    ~rtos_BlinkingWidget();
+    void start_blinking();
+    void stop_blinking();
+    void setup_blinking(rtos_Blinker *blinker);
+    virtual void save_canvas_color() = 0;
+    virtual void restore_canvas_color() = 0;
+    virtual void blink() = 0;
+    virtual void show_focus() = 0;
+};
+
+
 
 class rtos_Model;
 
